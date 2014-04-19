f175f010bdc11e4b682aef1d67901910c2f9f495
Arne Schauf <git@as-webservices.de>
2014-04-20 01:44:20 +0200

    Screenshots from example project

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -3,9 +3,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from things.views import CreateThingView
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'example_project.views.home', name='home'),
+    url(r'^$', CreateThingView.as_view(), name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
